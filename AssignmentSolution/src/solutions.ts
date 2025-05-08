e42e2c9567c274a492635c687dd81db9a0823964
unknown <mehedytouhidurzaman@gmail.com>
2025-05-08 19:52:00 +0600

    The problem 5 and 6 is completed

--- AssignmentSolution/src/solutions.ts
+++ AssignmentSolution/src/solutions.ts
@@ -30,3 +30,40 @@ const result3 = concatenateArrays([1, 2], [3, 4], [5]);
 console.log(result3)
 
 
+function processValue(value: string | number): number {
+
+  if (typeof value === "string") {
+    return value.length   
+  }
+  else{
+     return value*2
+  }
+}
+
+const result5 = processValue(10);
+console.log(result5)
+
+interface Product {
+  name: string;
+  price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null{
+  if(products.length === 0) return null
+  let highestPrice = products[0]
+  for(const product of products){
+    if(product.price>highestPrice.price){
+      highestPrice = product
+    }
+  }
+  return highestPrice
+}
+
+const products = [
+  { name: "Pen", price: 10 },
+  { name: "Notebook", price: 25 },
+  { name: "Bag", price: 50 }
+];
+
+const result6 = getMostExpensiveProduct(products)
+console.log(result6)
