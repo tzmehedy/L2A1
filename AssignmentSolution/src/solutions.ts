339100ffcfe25c7300213f43a31045af21e0e56e
unknown <mehedytouhidurzaman@gmail.com>
2025-05-08 13:27:16 +0600

    The second problem solution is completed

--- AssignmentSolution/src/solutions.ts
+++ AssignmentSolution/src/solutions.ts
@@ -6,3 +6,18 @@ function formatString(input:string, toUpper?:boolean):string{
 
 const result1 = formatString("Hello", false);
 console.log(result1)
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[]{
+
+    return items.filter((e) => e.rating >= 4);
+}
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+
+const result2 = filterByRating(books)
+console.log(result2)
